d869358caa8582ee96f6c85aaa66b7d65ead15c2
LuizGarbini <garbasneto@gmail.com>
2023-09-15 12:56:34 -0300

    upload video finished

--- src/routes/upload-video.ts
+++ src/routes/upload-video.ts
@@ -29,10 +29,21 @@ export async function uploadVideoRoute(app: FastifyInstance ) {
         return reply.status(400).send({error: 'Invalid input type, please upload a MP3' })
       }
 
-      const fileBaseName = path.basename(data.fieldname, extension) 
-      const fileUploadName = `${fileBaseName}-{randomUUID()}${extension}`
+      const fileBaseName = path.basename(data.filename, extension) 
+      const fileUploadName = `${fileBaseName}-${randomUUID()}${extension}`
       const uploadDestination = path.resolve(__dirname, '../../tmp', fileUploadName)
 
       await pump(data.file, fs.createWriteStream(uploadDestination))
+
+      const video = await prisma.video.create({
+        data: {
+          name: data.filename,
+          path: uploadDestination,
+        }
+      })
+
+      return {
+        video,
+      }
     })
 }
