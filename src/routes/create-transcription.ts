0f08ba6aa0ecee858ee84ad595e5c191456bb749
LuizGarbini <garbasneto@gmail.com>
2023-09-17 11:20:54 -0300

    description return finished

--- src/routes/create-transcription.ts
+++ src/routes/create-transcription.ts
@@ -1,6 +1,8 @@
 import { FastifyInstance } from "fastify";
+import { createReadStream } from "node:fs";
 import { z } from 'zod'
 import { prisma } from "../lib/prisma";
+import { openai } from "../lib/openai";
 
 export async function createTranscriptionRoute(app: FastifyInstance ) {
     app.post('/videos/:videoId/transcription', async (req) => {
@@ -23,11 +25,27 @@ export async function createTranscriptionRoute(app: FastifyInstance ) {
         })
 
         const videoPath = video.path
+        const audioReadStream = createReadStream(videoPath)
 
-        return {
-            videoId,
+        const response = await openai.audio.transcriptions.create({
+            file: audioReadStream,
+            model: 'whisper-1',
+            language: 'pt',
+            response_format: 'json',
+            temperature: 0,
             prompt,
-            videoPath
-        }
+        })
+
+        const transcription = response.text
+
+        await prisma.video.update({
+            where: {
+                id: videoId,
+            },
+            data: {
+                transcription,
+            }
+        })
+        return { transcription }
     })
 }
